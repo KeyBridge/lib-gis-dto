e31c7003fc2fe574f6118a74c19b14257c81cd5c
Jesse Caulfield <jesse.caulfield@keybridgeglobal.com>
2018-08-04 23:09:36 -0400

    add do-not-use notice

--- src/main/java/ch/keybridge/lib/gis/dto/package-info.java
+++ src/main/java/ch/keybridge/lib/gis/dto/package-info.java
@@ -1,2 +1,5 @@
+/**
+ * 08/04/18 coding practice: DTO classes MUST NOT declare a namespace.
+ */
 //@javax.xml.bind.annotation.XmlSchema(namespace = "http://keybridge.ch/gis/dto")
 package ch.keybridge.lib.gis.dto;
