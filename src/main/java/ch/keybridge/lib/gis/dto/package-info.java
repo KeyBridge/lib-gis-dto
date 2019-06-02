5759afcc982b5550bacedf7a7632e45e2592b19a
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2019-06-02 16:28:26 -0400

    v3.3.0 - rename AbstractGISFeature to AbstractFeature; add package-info with namespace "http://keybridge.ch/gis/dto"

--- src/main/java/ch/keybridge/lib/gis/dto/package-info.java
+++ src/main/java/ch/keybridge/lib/gis/dto/package-info.java
@@ -1,5 +1,5 @@
 /**
  * 08/04/18 coding practice: DTO classes MUST NOT declare a namespace.
  */
-//@javax.xml.bind.annotation.XmlSchema(namespace = "http://keybridge.ch/gis/dto")
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://keybridge.ch/gis/dto")
 package ch.keybridge.lib.gis.dto;
